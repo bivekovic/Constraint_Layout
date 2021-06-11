4344f59028296d77030909aead7fe8b23b6199b4
benjamin.ivekovic <Dipling12>
2021-06-11 21:18:12 +0200

    Konacna verzija 11.06.

--- app/src/main/java/com/algebra/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/algebra/constraintlayout/MainActivity.kt
@@ -2,12 +2,72 @@ package com.algebra.constraintlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import java.util.regex.Pattern
+
+/*
+Validiraj da je uneseno neko ime
+Validiraj da je uneseno neko prezime
+Validiraj da je odabrana neka opcija za spol
+Validiraj da je unesen mail i da je u ispravnom formatu
+
+Korisniku pokažite odgovarajuću poruku (Toast)
+ */
 
 class MainActivity : AppCompatActivity( ) {
+
+    private lateinit var etIme : EditText
+    private lateinit var etPrezime : EditText
+    private lateinit var etMail : EditText
+    private lateinit var radioGrupa : RadioGroup
+    private lateinit var radioM : RadioButton
+    private lateinit var radioZ : RadioButton
+    private lateinit var radioNZR : RadioButton
+    private lateinit var bOdustani : Button
+    private lateinit var bSpremi : Button
+
+
+
     override fun onCreate( savedInstanceState: Bundle? ) {
         super.onCreate( savedInstanceState )
         setContentView( R.layout.activity_main )
 
-        // komentar za probu
+        initWidgets( )
+        setupListeners( )
+    }
+
+    fun setupListeners( ) {
+        bOdustani.setOnClickListener { }
+        bSpremi.setOnClickListener {
+            val unesenoIme = etIme.text.toString( );
+            val unesenoPrezime = etPrezime.text.toString( );
+            var spol = ""
+            if( radioM.isChecked ) spol="M"
+            else if( radioZ.isChecked ) spol = "Z"
+            else if( radioNZR.isChecked ) spol = "/"
+            var mail = etMail.text.toString( )
+            var korisnik = Korisnik( unesenoIme, unesenoPrezime, mail, spol )
+            Toast.makeText( this, korisnik.toString( ), Toast.LENGTH_LONG ).show( )
+        }
+    }
+
+    fun initWidgets( ) {
+        etIme      = findViewById( R.id.etIme )
+        etPrezime  = findViewById( R.id.etPrezime )
+        etMail     = findViewById( R.id.etMail )
+        radioGrupa = findViewById( R.id.radiogrupa )
+        radioM     = findViewById( R.id.rbM )
+        radioZ     = findViewById( R.id.rbZ )
+        radioNZR   = findViewById( R.id.rbNZR )
+        bOdustani  = findViewById( R.id.bOdustani )
+        bSpremi    = findViewById( R.id.bSpremi )
+    }
+
+    fun isValidEmail( mail:String ): Boolean {
+        val email_pattern = "^[a-zA-Z0-9#_~!$&'()*+,;=:.\"(),:;<>@\\[\\]\\\\]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*$"
+        val pattern = Pattern.compile(email_pattern)
+        return pattern.matcher( mail ).matches()
     }
 }
+
+data class Korisnik( var ime:String, var prezime:String, var mail:String, var spol: String )
