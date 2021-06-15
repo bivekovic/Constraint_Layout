102dd19e9cefb247b396075cbd1e425923f6ed01
bivekovic <bla@bla.com>
2021-06-15 17:09:35 +0200

    vracam na staro

--- app/src/main/java/com/algebra/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/algebra/constraintlayout/MainActivity.kt
@@ -26,8 +26,6 @@ class MainActivity : AppCompatActivity( ) {
     private lateinit var bOdustani : Button
     private lateinit var bSpremi : Button
 
-    // jos nesto
-
 
 
     override fun onCreate( savedInstanceState: Bundle? ) {
